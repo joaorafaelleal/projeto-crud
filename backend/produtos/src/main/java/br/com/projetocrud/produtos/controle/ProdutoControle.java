bb04a2faddc39d0de90dc98cd8bf37f54b340366
jabes <jabesvieira4@gmail.com>
2025-03-31 12:26:50 -0300

    Rota para cadastrar e remover criadas

--- backend/produtos/src/main/java/br/com/projetocrud/produtos/controle/ProdutoControle.java
+++ backend/produtos/src/main/java/br/com/projetocrud/produtos/controle/ProdutoControle.java
@@ -1,37 +1,44 @@
 package br.com.projetocrud.produtos.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.com.projetocrud.produtos.modelo.ProdutoModelo;
+import br.com.projetocrud.produtos.modelo.RespostaModelo;
 import br.com.projetocrud.produtos.servico.ProdutoServico;
 
-
 @RestController
-
 public class ProdutoControle {
 
   @Autowired
-
   private ProdutoServico ps;
 
+  @DeleteMapping("/remover/{codigo}")
+  public ResponseEntity<RespostaModelo> remover(@PathVariable long codigo) {
+    return ps.remover(codigo);
+
+  }
+
+  @PostMapping("/cadastrar")
+  public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm) {
+    return ps.cadastrar(pm);
+
+  }
+
   @GetMapping("/listar")
-
   public Iterable<ProdutoModelo> listar() {
-
     return ps.listar();
 
   }
 
   @GetMapping("/")
-
   public String rota() {
-
     return "A API está funcionando!";
 
   }
-
 }
--- backend/produtos/src/main/java/br/com/projetocrud/produtos/servico/ProdutoServico.java
+++ backend/produtos/src/main/java/br/com/projetocrud/produtos/servico/ProdutoServico.java
@@ -1,9 +1,15 @@
 package br.com.projetocrud.produtos.servico;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.HttpStatus;
+
+import org.springframework.http.ResponseEntity;
+
 import org.springframework.stereotype.Service;
 
 import br.com.projetocrud.produtos.modelo.ProdutoModelo;
+import br.com.projetocrud.produtos.modelo.RespostaModelo;
 import br.com.projetocrud.produtos.repositorio.ProdutoRepositorio;
 
 @Service
@@ -13,8 +19,34 @@ public class ProdutoServico {
   @Autowired
   private ProdutoRepositorio pr;
 
-  public Iterable<ProdutoModelo> listar(){
-        return pr.findAll();
+  @Autowired
+  private RespostaModelo rm;
+
+  public Iterable<ProdutoModelo> listar() {
+    return pr.findAll();
+
   }
 
+  public ResponseEntity<?> cadastrar(ProdutoModelo pm) {
+    if (pm.getNome().equals("")) {
+      rm.setResposta("O nome do produto é obrigatório!");
+      return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
+
+    } else if (pm.getMarca().equals("")) {
+      rm.setResposta("O nome da marca do produto é obrigratório!");
+      return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
+
+    } else {
+      return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.CREATED);
+    }
+
+  }
+  
+  public ResponseEntity<RespostaModelo> remover(long codigo) {
+    pr.deleteById(codigo);
+    rm.setResposta("O produto foi removido com sucesso!");
+    return new ResponseEntity<RespostaModelo>(rm, HttpStatus.OK);
+
+  }
+  
 }

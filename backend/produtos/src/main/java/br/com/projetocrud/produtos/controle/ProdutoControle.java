cbcb075eed59b9c1b7eab92ba5aff3197ba20b58
jabes <jabesvieira4@gmail.com>
2025-03-31 11:35:31 -0300

    Criação da rota /listar

--- backend/produtos/src/main/java/br/com/projetocrud/produtos/controle/ProdutoControle.java
+++ backend/produtos/src/main/java/br/com/projetocrud/produtos/controle/ProdutoControle.java
@@ -1,5 +1,37 @@
 package br.com.projetocrud.produtos.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.projetocrud.produtos.modelo.ProdutoModelo;
+import br.com.projetocrud.produtos.servico.ProdutoServico;
+
+
+@RestController
+
 public class ProdutoControle {
-  
+
+  @Autowired
+
+  private ProdutoServico ps;
+
+  @GetMapping("/listar")
+
+  public Iterable<ProdutoModelo> listar() {
+
+    return ps.listar();
+
+  }
+
+  @GetMapping("/")
+
+  public String rota() {
+
+    return "A API está funcionando!";
+
+  }
+
 }

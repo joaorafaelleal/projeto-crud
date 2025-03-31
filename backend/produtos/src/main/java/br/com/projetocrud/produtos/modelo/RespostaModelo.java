1af14f2fd7517524113206a85bb0a86a1f617c6d
jabes <jabesvieira4@gmail.com>
2025-03-31 11:33:27 -0300

    Criação da classe RespostaModelo

---
+++ backend/produtos/src/main/java/br/com/projetocrud/produtos/modelo/RespostaModelo.java
@@ -0,0 +1,14 @@
+package br.com.projetocrud.produtos.modelo;
+
+import org.springframework.stereotype.Component;
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@Setter
+@Getter
+public class RespostaModelo {
+
+  private String resposta;
+
+}

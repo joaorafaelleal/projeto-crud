18fe4a0d3c34dabf5bb9a99ec9e25d31c80837fe
joaorafaelleal <joao.cavalcante91@aluno.ce.gov.br>
2025-04-12 16:22:47 -0300

    Projeto front criado

--- backend/produtos/src/main/java/br/com/projetocrud/produtos/controle/ProdutoControle.java
+++ backend/produtos/src/main/java/br/com/projetocrud/produtos/controle/ProdutoControle.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import br.com.projetocrud.produtos.modelo.ProdutoModelo;
@@ -16,31 +17,32 @@ import br.com.projetocrud.produtos.servico.ProdutoServico;
 @RestController
 @CrossOrigin(origins = "*")
 public class ProdutoControle {
-
+  // Injeção de dependências
   @Autowired
   private ProdutoServico ps;
 
   @DeleteMapping("/remover/{codigo}")
   public ResponseEntity<RespostaModelo> remover(@PathVariable long codigo) {
     return ps.remover(codigo);
-
   }
 
   @PostMapping("/cadastrar")
   public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm) {
-    return ps.cadastrar(pm);
+    return ps.cadastrarAlterar(pm, "cadastrar");
+  }
 
+  @PutMapping("/alterar")
+  public ResponseEntity<?> alterar(@RequestBody ProdutoModelo pm) {
+    return ps.cadastrarAlterar(pm, "alterar");
   }
 
   @GetMapping("/listar")
   public Iterable<ProdutoModelo> listar() {
     return ps.listar();
-
   }
 
   @GetMapping("/")
   public String rota() {
     return "A API está funcionando!";
-
   }
 }
--- backend/produtos/src/main/java/br/com/projetocrud/produtos/servico/ProdutoServico.java
+++ backend/produtos/src/main/java/br/com/projetocrud/produtos/servico/ProdutoServico.java
@@ -13,7 +13,6 @@ import br.com.projetocrud.produtos.modelo.RespostaModelo;
 import br.com.projetocrud.produtos.repositorio.ProdutoRepositorio;
 
 @Service
-
 public class ProdutoServico {
 
   @Autowired
@@ -22,31 +21,32 @@ public class ProdutoServico {
   @Autowired
   private RespostaModelo rm;
 
+  // Listar produtos
   public Iterable<ProdutoModelo> listar() {
     return pr.findAll();
-
   }
 
-  public ResponseEntity<?> cadastrar(ProdutoModelo pm) {
+  public ResponseEntity<?> cadastrarAlterar(ProdutoModelo pm, String acao) {
     if (pm.getNome().equals("")) {
       rm.setResposta("O nome do produto é obrigatório!");
       return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
-
     } else if (pm.getMarca().equals("")) {
-      rm.setResposta("O nome da marca do produto é obrigratório!");
+      rm.setResposta("A marca do produto é obrigatório!");
       return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
-
     } else {
-      return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.CREATED);
-    }
+      if (acao.equals("cadastrar")) {
+        return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.CREATED);
+      } else {
+        return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.OK);
 
+      }
+    }
   }
-  
+
   public ResponseEntity<RespostaModelo> remover(long codigo) {
     pr.deleteById(codigo);
     rm.setResposta("O produto foi removido com sucesso!");
     return new ResponseEntity<RespostaModelo>(rm, HttpStatus.OK);
-
   }
-  
+
 }

f8295a6a6260769ea7be07e7083b836adf508cd2
jabez <jabesvieira4@gmail.com>
2025-04-29 12:02:06 -0300

    Correção da tabela

--- frontend/produtos/src/Tabela.jsx
+++ frontend/produtos/src/Tabela.jsx
@@ -16,7 +16,7 @@ function Tabela({ vetor }) {
             <td>{obj.nome}</td>
             <td>{obj.marca}</td>
             <td>
-              <button className="btn btn-success"></button>
+              <button className="btn btn-success">Selecionar</button>
             </td>
           </tr>
         ))}
